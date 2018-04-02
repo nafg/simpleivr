7cd686d8b55113451cedfcf7a9384dcdfbf1578c
Naftoli Gugenheim <naftoligug@gmail.com>
2018-04-02 00:48:57 -0400

    0.2.2: make IvrStepRunner non-final (again)

--- core/src/main/scala/simpleivr/IvrStepRunner.scala
+++ core/src/main/scala/simpleivr/IvrStepRunner.scala
@@ -3,7 +3,7 @@ package simpleivr
 import cats.effect.IO
 
 
-final class IvrStepRunner(commandInterpreter: IvrCommandInterpreter) {
+class IvrStepRunner(commandInterpreter: IvrCommandInterpreter) {
   def runIvrCommand[A](cmd: IvrCommandF[A]): IO[A] = cmd.fold[IO](commandInterpreter)
   def runIvrStep[A](step: IvrStep[A]): IO[A] = step.runM(runIvrCommand)
 }
