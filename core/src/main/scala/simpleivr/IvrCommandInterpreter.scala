8c2aba44226657ba5ea6754c6f3452dace13af86
Naftoli Gugenheim <naftoligug@gmail.com>
2018-04-13 16:34:53 -0400

    Factor out SayIvrCommandInterpreter

--- core/src/main/scala/simpleivr/IvrCommandInterpreter.scala
+++ core/src/main/scala/simpleivr/IvrCommandInterpreter.scala
@@ -5,62 +5,9 @@ import java.io.File
 import cats.effect.IO
 
 
-class IvrCommandInterpreter(ivrApi: IvrApi, speakGenerator: SpeakGenerator = Text2waveSpeakGenerator)
-  extends IvrCommand.Folder[IO] {
+class IvrCommandInterpreter(ivrApi: IvrApi, val speakGenerator: SpeakGenerator = Text2waveSpeakGenerator)
+  extends SayIvrCommandInterpreter {
 
-  private def curTime = IO(System.currentTimeMillis())
-
-  protected def runPause(ms: Int, interrupt: String): IO[Option[Char]] =
-    if (ms <= 0)
-      IO.pure(None)
-    else if (interrupt.isEmpty)
-      IO(Thread.sleep(ms)).map(_ => None)
-    else
-      for {
-        startTime <- curTime
-        digit <- waitForDigit(ms)
-        res <-
-          if (digit.exists(interrupt.contains(_)))
-            IO.pure(digit)
-          else
-            curTime.map(_ - startTime).flatMap(elapsed => runPause(ms - elapsed.toInt, interrupt))
-      } yield res
-
-  /**
-    * `None` if no DTMF was received, otherwise `Some(d)` where `d` is the
-    * digit that was pressed.
-    */
-  def runSayable(sayable: Sayable, interrupt: String): IO[Option[Char]] = sayable match {
-    case SayNothing =>
-      IO.pure(None)
-
-    case Pause(ms) => runPause(ms, interrupt)
-
-    case play: Play =>
-      streamFile(play.path.pathAndName, interrupt).map {
-        case 0 => None
-        case c => Some(c)
-      }
-
-    case speak: Sayables#Speak =>
-      speakGenerator(speak)
-        .flatMap { _ =>
-        println("Speaking: " + speak.msg)
-        runSayable(Play(speak.path), interrupt)
-      }
-
-    case SayableSeq(messages) =>
-      def loop(sayables: List[Sayable]): IO[Option[Char]] = sayables match {
-        case Nil         => IO.pure(None)
-        case msg :: msgs =>
-          runSayable(msg, interrupt).flatMap {
-            case Some(c) => IO.pure(Some(c))
-            case None    => loop(msgs)
-          }
-      }
-
-      loop(messages)
-  }
   override def streamFile(pathAndName: String, interruptChars: String) = IO {
     ivrApi.streamFile(pathAndName, interruptChars)
   }
@@ -88,7 +35,6 @@ class IvrCommandInterpreter(ivrApi: IvrApi, speakGenerator: SpeakGenerator = Tex
     ivrApi.hangup()
   }
   override def setAutoHangup(seconds: Int) = ivrApi.setAutoHangup(seconds)
-  override def say(sayable: Sayable, interruptDigits: String) = runSayable(sayable, interruptDigits)
   override def originate(dest: String, script: String, args: Seq[String]) = IO {
     ivrApi.originate(dest, script, args)
   }
---
+++ core/src/main/scala/simpleivr/SayIvrCommandInterpreter.scala
@@ -0,0 +1,64 @@
+package simpleivr
+
+import cats.effect.IO
+
+
+trait SayIvrCommandInterpreter extends IvrCommand.Folder[IO] {
+  def speakGenerator: SpeakGenerator
+
+  private def curTime = IO(System.currentTimeMillis())
+
+  protected def runPause(ms: Int, interrupt: String): IO[Option[Char]] =
+    if (ms <= 0)
+      IO.pure(None)
+    else if (interrupt.isEmpty)
+      IO(Thread.sleep(ms)).map(_ => None)
+    else
+      for {
+        startTime <- curTime
+        digit <- waitForDigit(ms)
+        res <-
+          if (digit.exists(interrupt.contains(_)))
+            IO.pure(digit)
+          else
+            curTime.map(_ - startTime).flatMap(elapsed => runPause(ms - elapsed.toInt, interrupt))
+      } yield res
+
+  /**
+    * `None` if no DTMF was received, otherwise `Some(d)` where `d` is the
+    * digit that was pressed.
+    */
+  def runSayable(sayable: Sayable, interrupt: String): IO[Option[Char]] = sayable match {
+    case SayNothing =>
+      IO.pure(None)
+
+    case Pause(ms) => runPause(ms, interrupt)
+
+    case play: Play =>
+      streamFile(play.path.pathAndName, interrupt).map {
+        case 0 => None
+        case c => Some(c)
+      }
+
+    case speak: Sayables#Speak =>
+      speakGenerator(speak)
+        .flatMap { _ =>
+          println("Speaking: " + speak.msg)
+          runSayable(Play(speak.path), interrupt)
+        }
+
+    case SayableSeq(messages) =>
+      def loop(sayables: List[Sayable]): IO[Option[Char]] = sayables match {
+        case Nil         => IO.pure(None)
+        case msg :: msgs =>
+          runSayable(msg, interrupt).flatMap {
+            case Some(c) => IO.pure(Some(c))
+            case None    => loop(msgs)
+          }
+      }
+
+      loop(messages)
+  }
+
+  override def say(sayable: Sayable, interruptDigits: String) = runSayable(sayable, interruptDigits)
+}
